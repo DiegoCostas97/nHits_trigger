3cf61186e1bc9d0efb285a4a80a4982622d606cd
Diego Costas Rodriguez <dcostasr@lxplus926.cern.ch>
2025-06-06 10:04:01 +0200

    Changes in nHits function. Now we can trigger in single events or in multiple events, depending on the mode variable. Also changes in the function that processes the data, we need to specify the mcc_map if we're dealing with non-calibreated data. This is not correctly done, but is fine for the moment.

--- src/read_data.py
+++ src/read_data.py
@@ -34,29 +34,44 @@ def read_file(files, parts, evt_offset):
 
 def process_and_write_parts(run_files, good_parts, mcc_map, max_card, max_chan, outdir="tmp_parquet"):
     os.makedirs(outdir, exist_ok=True)
+
+    # Remove Directories If Run Is Repeated
+    for f in glob.glob(f"{outdir}/*.parquet"):
+        os.remove(f)
+
     evt_offset = 0
 
     for i, part in enumerate(tqdm(good_parts, desc="Processing parts")):
         file_hit_card_ids, file_hit_channel_ids, file_hit_times, file_hit_charges, file_event_number, file_window_time, file_hit_slot_ids, file_hit_position_ids = read_file(run_files, part, evt_offset)
 
-        # Build lookup
-        lookup = np.zeros((max_card + 1, max_chan + 1))
-        for (card, chan), shift in mcc_map.items():
-            lookup[card, chan] = shift
+        if mcc_map != None:
+            # Build lookup
+            lookup = np.zeros((max_card + 1, max_chan + 1))
+            for (card, chan), shift in mcc_map.items():
+                lookup[card, chan] = shift
 
-        # Hit Times Correction
-        flat_card_ids     = ak.ravel(file_hit_card_ids)
-        flat_chan_ids     = ak.ravel(file_hit_channel_ids)
-        flat_corrections  = lookup[flat_card_ids, flat_chan_ids]
-        corrections       = ak.unflatten(flat_corrections, ak.num(file_hit_card_ids))
+            # Hit Times Correction
+            flat_card_ids     = ak.ravel(file_hit_card_ids)
+            flat_chan_ids     = ak.ravel(file_hit_channel_ids)
+            flat_corrections  = lookup[flat_card_ids, flat_chan_ids]
+            corrections       = ak.unflatten(flat_corrections, ak.num(file_hit_card_ids))
 
-        term1 = file_hit_times
-        # term2 = (file_event_number // 512) * (2**35)
-        # term3 = ((file_event_number % 512 == 511) & (file_hit_times < 2**34)) * (2**35)
-        term4 = file_window_time
-        # corrected_times = term1 + term2 + term3 + corrections
-        corrected_times = term1 + term4 + corrections 
+            term1 = file_hit_times
+            # term2 = (file_event_number // 512) * (2**35)
+            # term3 = ((file_event_number % 512 == 511) & (file_hit_times < 2**34)) * (2**35)
+            term4 = file_window_time
+            # corrected_times = term1 + term2 + term3 + corrections
+            corrected_times = term1 + term4 + corrections 
+        
+        elif mcc_map == None:
 
+            term1 = file_hit_times
+            # term2 = (file_event_number // 512) * (2**35)
+            # term3 = ((file_event_number % 512 == 511) & (file_hit_times < 2**34)) * (2**35)
+            term4 = file_window_time
+            # corrected_times = term1 + term2 + term3 + corrections
+            corrected_times = term1 + term4
+            
         # Save to Disk
         ak.to_parquet(file_hit_card_ids,     f"{outdir}/card_ids_part{i}.parquet")
         ak.to_parquet(file_hit_channel_ids,  f"{outdir}/channel_ids_part{i}.parquet")
@@ -64,6 +79,7 @@ def process_and_write_parts(run_files, good_parts, mcc_map, max_card, max_chan, 
         ak.to_parquet(file_hit_position_ids, f"{outdir}/position_ids_part{i}.parquet")
         ak.to_parquet(file_hit_charges,      f"{outdir}/charges_part{i}.parquet")
         ak.to_parquet(file_event_number,     f"{outdir}/event_number_part{i}.parquet")
+        ak.to_parquet(file_window_time,      f"{outdir}/window_time_part{i}.parquet")
         ak.to_parquet(corrected_times,       f"{outdir}/hit_times_part{i}.parquet")
 
         evt_offset = file_event_number[-1][-1] + 1
@@ -81,6 +97,7 @@ def load_concatenated(outdir="./tmp_parquet"):
         "charges":      load("charges_part*.parquet"),
         "event_number": load("event_number_part*.parquet"),
         "hit_times":    load("hit_times_part*.parquet"),
+        "window_time":  load("window_time_part*.parquet")
     }
 
 def read_mcc_offsets(path='/eos/home-d/dcostasr/SWAN_projects/NiCf/offline_trigger/mmc_map_R1609.json'):
@@ -105,7 +122,8 @@ def read_parquet(data, mask=True):
         run_positions = data["position_ids"][mask]
         run_times     = data["hit_times"]   [mask]   
         run_events    = data["event_number"][mask]
-        run_charges   = data["charges"]     [mask] 
+        run_charges   = data["charges"]     [mask]
+        run_window_times = data["window_time"][mask]
 
     else:
         run_cards     = data["card_ids"]
@@ -114,19 +132,19 @@ def read_parquet(data, mask=True):
         run_positions = data["position_ids"]  
         run_times     = data["hit_times"]      
         run_events    = data["event_number"]
-        run_charges   = data["charges"]      
+        run_charges   = data["charges"]   
+        run_window_times = data["window_time"]   
 
-    return run_cards, run_channels, run_slots, run_positions, run_times, run_events, run_charges
+    return run_cards, run_channels, run_slots, run_positions, run_times, run_events, run_charges, run_window_times
 
-def nHits(hit_times, w, thresh_min, thresh_max, pre_window, post_window, jump, progress_bar=True):
-    nevents = len(hit_times)
-    triggered_hits_index = {}
+def nHits(mode, hit_times, w, thresh_min, thresh_max, pre_window, post_window, jump, event=0, progress_bar=True):
+    
+    # SINGLE EVENT MODE
+    if mode=="single_event":
+        triggered_hits_index = {}
 
-    for event in tqdm(range(nevents), total=nevents, leave=progress_bar):
         ht = ak.to_numpy(hit_times[event])
-        if len(ht) == 0:
-            continue
-        
+
         # Sliding window logic
         ends   = ht + w
         right  = np.searchsorted(ht, ends, side="left")
@@ -135,8 +153,6 @@ def nHits(hit_times, w, thresh_min, thresh_max, pre_window, post_window, jump, p
 
         # Get the indices of all the hit times that triggered the nHits algorithm
         trigger_indices = np.where((counts >= thresh_min) & (counts < thresh_max))[0]
-        if len(trigger_indices) == 0: # Skip if no triggers in event
-            continue
 
         event_hits = []
         last_trigger_time = -np.inf  # First trigger always need to exist
@@ -167,5 +183,58 @@ def nHits(hit_times, w, thresh_min, thresh_max, pre_window, post_window, jump, p
         # Update dictionary
         if len(event_hits) > 0:
             triggered_hits_index[event] = event_hits
+
+    # MULTIPLE EVENTS MODE
+    elif mode == "multiple_events":
+        nevents = len(hit_times)
+        triggered_hits_index = {}
+
+        for event in tqdm(range(nevents), total=nevents, leave=progress_bar):
+            ht = ak.to_numpy(hit_times[event])
+            if len(ht) == 0:
+                continue
+            
+            # Sliding window logic
+            ends   = ht + w
+            right  = np.searchsorted(ht, ends, side="left")
+            left   = np.arange(len(ht))
+            counts = right - left
+
+            # Get the indices of all the hit times that triggered the nHits algorithm
+            trigger_indices = np.where((counts >= thresh_min) & (counts < thresh_max))[0]
+            if len(trigger_indices) == 0: # Skip if no triggers in event
+                continue
+
+            event_hits = []
+            last_trigger_time = -np.inf  # First trigger always need to exist
+
+            # Search for the rest of the hits in the trigger
+            for idx in trigger_indices:
+                # Define actual sliding window that triggered
+                i_start = idx
+                i_end = right[idx]  # one past the last hit in window
+                window_hits = ht[i_start:i_end]
+
+                first_hit_time = window_hits[0]
+                last_hit_time  = window_hits[-1]
+
+                # Avoid overlap due to dead time
+                if first_hit_time < last_trigger_time + jump:
+                    continue
+
+                # Expand window
+                t_min = first_hit_time - pre_window
+                t_max = last_hit_time + post_window
+
+                indices_in_window = np.where((ht >= t_min) & (ht <= t_max))[0]
+                event_hits.append(indices_in_window)
+
+                last_trigger_time = first_hit_time  # or maybe last_hit_time 
+
+            # Update dictionary
+            if len(event_hits) > 0:
+                triggered_hits_index[event] = event_hits
+    else:
+        print("enter a valid mode name")
 
     return triggered_hits_index
